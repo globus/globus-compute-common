85affd00c5305078ca7f715d8b11068e9a8bdb0c
Stephen Rosen <sirosen@globus.org>
2021-08-17 22:08:01 +0000

    Expand to complete test coverage (by lines)

--- tests/unit/test_response_errors.py
+++ tests/unit/test_response_errors.py
@@ -1,8 +1,40 @@
+import uuid
+
+import pytest
+
 from funcx_common.response_errors import (
+    AuthGroupNotFound,
+    ContainerNotFound,
+    EndpointAccessForbidden,
+    EndpointAlreadyRegistered,
+    EndpointNotFound,
+    EndpointOutdated,
+    EndpointStatsError,
+    ForwarderContactError,
+    ForwarderRegistrationError,
+    FunctionAccessForbidden,
+    FunctionNotFound,
+    FunctionNotPermitted,
     FuncxResponseError,
+    InternalError,
+    InvalidUUID,
+    LivenessStatsError,
+    RequestKeyError,
+    RequestMalformed,
     ResponseErrorCode,
+    TaskGroupAccessForbidden,
+    TaskGroupNotFound,
+    TaskNotFound,
+    UserNotFound,
     UserUnauthenticated,
 )
+
+
+def _check_pack_and_unpack(orig):
+    packed = orig.pack()
+    unpacked = FuncxResponseError.unpack(packed)
+    assert isinstance(unpacked, orig.__class__)
+    assert unpacked.reason == orig.reason
 
 
 def test_pack_error():
@@ -14,9 +46,72 @@ def test_pack_error():
     assert data["http_status_code"] == 401
 
 
-def test_unpack_inverts_pack():
-    packed = UserUnauthenticated().pack()
-    assert isinstance(FuncxResponseError.unpack(packed), UserUnauthenticated)
+@pytest.mark.parametrize("err_cls", [UserUnauthenticated])
+def test_unpack_inverts_pack_noargs(err_cls):
+    _check_pack_and_unpack(err_cls())
+
+
+@pytest.mark.parametrize(
+    "err_cls",
+    [
+        UserNotFound,
+        ForwarderRegistrationError,
+        ForwarderContactError,
+        RequestKeyError,
+        RequestMalformed,
+        InternalError,
+        EndpointOutdated,
+        InvalidUUID,
+    ],
+)
+def test_unpack_inverts_pack_single_strarg(err_cls):
+    _check_pack_and_unpack(err_cls("EXTERMINATE"))  # The Doctor
+
+
+@pytest.mark.parametrize(
+    "err_cls",
+    [
+        FunctionNotFound,
+        EndpointNotFound,
+        ContainerNotFound,
+        TaskNotFound,
+        AuthGroupNotFound,
+        FunctionAccessForbidden,
+        EndpointAccessForbidden,
+        EndpointAlreadyRegistered,
+        TaskGroupNotFound,
+        TaskGroupAccessForbidden,
+    ],
+)
+def test_unpack_inverts_pack_single_uuidarg(err_cls):
+    dummy_uuid = uuid.uuid1()
+    _check_pack_and_unpack(err_cls(dummy_uuid))
+    _check_pack_and_unpack(err_cls(str(dummy_uuid)))
+
+
+def test_unpack_inverts_pack_function_not_permitted():
+    dummy_uuid1 = uuid.uuid1()
+    dummy_uuid2 = uuid.uuid1()
+    _check_pack_and_unpack(FunctionNotPermitted(dummy_uuid1, dummy_uuid2))
+    _check_pack_and_unpack(FunctionNotPermitted(str(dummy_uuid1), dummy_uuid2))
+    _check_pack_and_unpack(FunctionNotPermitted(dummy_uuid1, str(dummy_uuid2)))
+    _check_pack_and_unpack(FunctionNotPermitted(str(dummy_uuid1), str(dummy_uuid2)))
+
+
+def test_unpack_inverts_pack_endpoint_stats():
+    dummy_uuid = uuid.uuid1()
+    _check_pack_and_unpack(
+        EndpointStatsError(dummy_uuid, "You will be assimilated.")
+    )  # The Enterprise
+    _check_pack_and_unpack(
+        EndpointStatsError(str(dummy_uuid), "You will be assimilated.")
+    )
+
+
+@pytest.mark.parametrize("status_code", [500, 400, 307])
+def test_unpack_inverts_pack_liveness_stats(status_code):
+    _check_pack_and_unpack(LivenessStatsError(status_code))
+    _check_pack_and_unpack(LivenessStatsError(str(status_code)))
 
 
 def test_default_init_uses_reasonstr(monkeypatch):
