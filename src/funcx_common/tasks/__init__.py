56e17ccdcead3f82a8c3ce1b942b0c4dc503c519
Stephen Rosen <sirosen@globus.org>
2021-12-20 20:45:26 +0000

    Add `RedisTask` implementation to library
    This is an implementation of the RedisTask class found in
    funcx-web-service, funcx-websocket-service, and funcx-forwarder
    
    In order to do this, InternalTaskState must be added to the task
    package.

--- src/funcx_common/tasks/__init__.py
+++ src/funcx_common/tasks/__init__.py
@@ -1,7 +1,8 @@
-from .constants import TaskState
+from .constants import InternalTaskState, TaskState
 from .protocol import TaskProtocol
 
 __all__ = (
     "TaskState",
+    "InternalTaskState",
     "TaskProtocol",
 )
