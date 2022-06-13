f12e0941e850e893a23c3d17efbacf1a86ac3a24
chris-janidlo <chris@globus.org>
2022-06-13 15:18:26 -0500

    Add `sdk_version_sharing` module (#46)

---
+++ tests/unit/test_sdk_version_sharing.py
@@ -0,0 +1,25 @@
+import pytest
+
+from funcx_common.sdk_version_sharing import (
+    FUNCX_VERSION_PREFIX,
+    parse_version_number,
+    user_agent_substring,
+)
+
+
+@pytest.mark.parametrize("version", ["foo", "1.0"])
+def test_user_agent_substring(version: str):
+    substring = user_agent_substring(version)
+    assert FUNCX_VERSION_PREFIX in substring
+    assert version in substring
+
+
+@pytest.mark.parametrize("version", ["foo", "1.0"])
+@pytest.mark.parametrize("template", ["{0}{1}", "bar/{0}{1}", "baz/{0}{1}/qux"])
+def test_parse_version_number(version: str, template: str):
+    user_agent_string = template.format(FUNCX_VERSION_PREFIX, version)
+    assert parse_version_number(user_agent_string) == version
+
+
+def test_parse_empty_version_number():
+    assert parse_version_number("foo") is None
