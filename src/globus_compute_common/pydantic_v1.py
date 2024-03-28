3e1709612492f7ca505828e78cc61d21f56d0464
Reid Mello <30907815+rjmello@users.noreply.github.com>
2024-03-27 22:45:10 -0400

    Enable Pydantic V2 support in setup.cfg
    We previously enabled support for Pydantic V2 but defaulted to installing
    V1 in `setup.cfg`, which could cause dependency conflicts with consuming
    packages. To address this, we have decided to extend the version range
    in `setup.cfg` to include V2.

--- src/globus_compute_common/pydantic_v1.py
+++ src/globus_compute_common/pydantic_v1.py
@@ -1,8 +1,8 @@
-"""Pydantic v2 provides access to the v1 API, enabling us to
-continue using v1 while allowing users to install v2 as needed.
+"""Pydantic V2 provides access to the V1 API, enabling us to
+continue using V1 while also supporting V2.
 """
 
 try:
     from pydantic.v1 import *  # noqa: F401 F403
 except ImportError:
-    from pydantic import *  # noqa: F401 F403
+    from pydantic import *  # type: ignore # noqa: F401 F403
